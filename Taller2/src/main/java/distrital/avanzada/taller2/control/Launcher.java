0bdab7d3880492e2763447264fb6576c5d4f8b27
Sebas <sdtrujillov-udistrital.edu.co>
2025-09-30 18:13:54 -0500

    Paquetes

---
+++ Taller2/src/main/java/distrital/avanzada/taller2/control/Launcher.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package distrital.avanzada.taller2.control;
+
+/**
+ *
+ * @author user
+ */
+public class Launcher {
+
+
+}
