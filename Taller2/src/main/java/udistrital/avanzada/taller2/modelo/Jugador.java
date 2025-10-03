18aff8627e1015d6d45f39e2ebe35eff28b5971f
ymbermudezt <230941078+ymbermudezt@users.noreply.github.com>
2025-10-03 15:03:05 -0500

    Actulizado Jugador.java quitado puntaje innecesario

--- Taller2/src/main/java/udistrital/avanzada/taller2/modelo/Jugador.java
+++ Taller2/src/main/java/udistrital/avanzada/taller2/modelo/Jugador.java
@@ -4,7 +4,8 @@ import java.io.Serializable;
 
 /**
  * Clase Jugador.java representa a los jugadores del juego Sus instancias seran
- * agrupadas en equipos de a 4
+ * agrupadas en equipos de a 4. Implementa Serializable para que sea serializado 
+ * cuando se serializen las listas de equipos
  *
  * @author Mauricio
  * @version 1.1
@@ -14,9 +15,7 @@ public class Jugador implements Serializable {
 
     private String nombre;
     private String apodo;
-    // Puntaje individual del jugador en la ronda actual
-    private transient int puntaje;
-
+    
     /**
      * Constructor con los parametros
      *
@@ -43,13 +42,5 @@ public class Jugador implements Serializable {
     public void setApodo(String apodo) {
         this.apodo = apodo;
     }
-
-    public int getPuntaje() {
-        return puntaje;
-    }
-
-    public void setPuntaje(int puntaje) {
-        this.puntaje = puntaje;
-    }
-
+    
 }
