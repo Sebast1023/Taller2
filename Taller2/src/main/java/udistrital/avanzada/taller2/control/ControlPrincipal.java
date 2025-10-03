f9be9fadf7fd9a4909dbc91f091609581175929f
YMB030 <129144487+YMB030@users.noreply.github.com>
2025-10-02 19:36:31 -0500

    Guardado de emergencia

--- Taller2/src/main/java/udistrital/avanzada/taller2/control/ControlPrincipal.java
+++ Taller2/src/main/java/udistrital/avanzada/taller2/control/ControlPrincipal.java
@@ -4,6 +4,9 @@
  */
 package udistrital.avanzada.taller2.control;
 
+import java.util.ArrayList;
+import java.util.Random;
+import udistrital.avanzada.taller2.modelo.Equipo;
 import udistrital.avanzada.taller2.vista.Ventana;
 
 /**
@@ -15,9 +18,39 @@ import udistrital.avanzada.taller2.vista.Ventana;
 public class ControlPrincipal {
 
     Ventana ventana;
+    private ControlEquipo controlEquipo;
+    private ControlTiro controlTiro;
+    private ArrayList<Equipo> empatados;
+    
 
     public ControlPrincipal() {
         this.ventana = new Ventana();
+        this.controlEquipo = new ControlEquipo();   
+        this.controlTiro = new ControlTiro();
     }
 
+    private void iniciarJuego(){
+        
+    }
+    
+    private void iniciarEquipos() {
+        //obtener datos del properties o serializador
+        //controlVentana.
+                
+    }
+    private void empate(){
+    
+    } 
+    /**
+     * Método para obtener numero random 
+     * @param limite
+     * @return 
+     */
+    private int obtenerNumeroRandom(int limite){
+        Random numAleatorio; 
+        numAleatorio = new Random(); 
+        int numero = numAleatorio.nextInt(limite+1) ;
+        return numero;
+    }
+    
 }
